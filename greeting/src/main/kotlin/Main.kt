3eac587c7ea773ad45aab1c79b02237fa27c6da0
Matthew Chin <matt@matthewjchin.com>
2024-05-15 20:41:19 -0700

    Added Kotlin greeting test files

---
+++ greeting/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+//fun main(args: Array<String>) {
+//    println("Hello Matt!")
+//
+//    // Try adding program arguments via Run/Debug configuration.
+//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+//    println("Program arguments: ${args.joinToString()}")
+//}
+
+fun main() {
+    println("What's your name?  ")
+    val name = readln()
+    println("Hello, $name!")
+
+}
+
+
